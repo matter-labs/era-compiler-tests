0e4b2626e4a589c39b609d7a2c8158c06de9e35a
Oleksandr Zarudnyi <hedgar2017@gmail.com>
2024-02-26 15:19:10 +0100

    Finalize the benchmark caller

--- solidity/complex/interpreter/BenchmarkCaller.sol
+++ solidity/complex/interpreter/BenchmarkCaller.sol
@@ -3,19 +3,18 @@
 pragma solidity >=0.8.24;
 
 contract BenchmarkCaller {
-    fallback(bytes calldata) external returns (bytes memory) {
+    function compare(address target1, address target2) external returns (uint256 gasDiff) {
         assembly {
-            let target := calldataload(0)
-            let startGas := gas()
-            let success := call(1, target, 0, 0, 0, 0, 0)
-            let endGas := gas()
-            let spentGas := sub(startGas, endGas)
+            let startGas1 := gas()
+            let success1 := call(startGas1, target1, 0, 0, 0, 0, 0)
+            let spentGas1 := sub(startGas1, gas())
 
-            mstore(0, spentGas)
-            if iszero(success) {
-                revert(0, 32)
-            }
-            return(0, 32)
+            let startGas2 := gas()
+            let success2 := call(startGas2, target2, 0, 0, 0, 0, 0)
+            let spentGas2 := sub(startGas2, gas())
+
+            gasDiff := sub(spentGas2, spentGas1)
         }
+        return gasDiff;
     }
 }
--- solidity/complex/interpreter/main.sol
+++ solidity/complex/interpreter/main.sol
@@ -2,23 +2,11 @@
 
 pragma solidity >=0.8.24;
 
-// "storage": {
-//     "0x0000000000000000000000000000000000008006": {
-//         "0x690612583382f3685b8ce5ac83aadb738903652422c990d64b61d18c89c8a9fa": "0x602A60005260006020F3",
-//         "0xbbf8b90d99e7c5d9e58b371467da43114f57564687a98b46cbc2a76bf21e3e90": "0x602A602A60005260006020F3"
-//     }
-// }
+import "./BenchmarkCaller.sol";
 
 contract Main {
-    fallback() external payable {
-        assembly {
-            calldatacopy(0, 0, calldatasize())
-            let success := call(shl(24, 1), 0xE594Ae1D7205E8e92fB22c59d040c31E1FcD139F, 0, 0, calldatasize(), 0, 0)
-            returndatacopy(0, 0, returndatasize())
-            if iszero(success) {
-                revert(0, returndatasize())
-            }
-            return(0, returndatasize())
-        }
+    function benchmark(address caller, address target1, address target2) external returns (uint256 gasDiff) {
+        BenchmarkCaller caller = BenchmarkCaller(caller);
+        gasDiff = caller.compare{gas: 2**24}(target1, target2);
     }
 }
