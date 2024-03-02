82354a9bc8e490ec1d189fb1e8e73dee88e66dac
Oleksandr Zarudnyi <hedgar2017@gmail.com>
2024-03-02 12:15:41 +0100

    Return the EraVM/EVM gas usage ratio

--- solidity/complex/interpreter/BenchmarkCaller.sol
+++ solidity/complex/interpreter/BenchmarkCaller.sol
@@ -3,18 +3,11 @@
 pragma solidity >=0.8.24;
 
 contract BenchmarkCaller {
-    function compare(address target1, address target2) external returns (uint256 gasDiff) {
+    function call(address target) external returns (uint256 spentGas) {
         assembly {
-            let startGas1 := gas()
-            let success1 := call(startGas1, target1, 0, 0, 0, 0, 0)
-            let spentGas1 := sub(startGas1, gas())
-
-            let startGas2 := gas()
-            let success2 := call(startGas2, target2, 0, 0, 0, 0, 0)
-            let spentGas2 := sub(startGas2, gas())
-
-            gasDiff := sub(spentGas2, spentGas1)
+            let startGas := gas()
+            let success := call(startGas, target, 0, 0, 0, 0, 0)
+            spentGas := sub(startGas, gas())
         }
-        return gasDiff;
     }
 }
--- solidity/complex/interpreter/main.sol
+++ solidity/complex/interpreter/main.sol
@@ -5,8 +5,19 @@ pragma solidity >=0.8.24;
 import "./BenchmarkCaller.sol";
 
 contract Main {
-    function benchmark(address caller, address target1, address target2) external returns (uint256 gasDiff) {
+    function benchmark(address caller, address target1, address target2) external returns (uint ratio) {
         BenchmarkCaller caller = BenchmarkCaller(caller);
-        gasDiff = caller.compare{gas: 2**24}(target1, target2);
+
+        uint gasEraVM0 = gasleft();
+        uint spentGasEVM1 = caller.call{gas: 2**24}(target1);
+        uint spentGasEraVM1 = gasEraVM0 - gasleft();
+
+        uint gasEraVM1 = gasleft();
+        uint spentGasEVM2 = caller.call{gas: 2**24}(target2);
+        uint spentGasEraVM2 = gasEraVM1 - gasleft();
+
+        uint gasDiffEVM = spentGasEVM2 - spentGasEVM1;
+        uint gasDiffEraVM = spentGasEraVM1 - spentGasEraVM2;
+        ratio = gasDiffEraVM / gasDiffEVM;
     }
 }
