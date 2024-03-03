377c1e2cde698042a55af3ec0fdaee0268173762
Oleksandr Zarudnyi <hedgar2017@gmail.com>
2024-03-03 22:44:16 +0400

    Move ergs measuring outside of the test

--- solidity/complex/interpreter/main.sol
+++ solidity/complex/interpreter/main.sol
@@ -5,19 +5,8 @@ pragma solidity >=0.8.24;
 import "./BenchmarkCaller.sol";
 
 contract Main {
-    function benchmark(address caller, address target1, address target2) external returns (uint ratio) {
+    function benchmark(address caller, address target) external returns (uint spentGasEVM) {
         BenchmarkCaller caller = BenchmarkCaller(caller);
-
-        uint gasEraVM0 = gasleft();
-        uint spentGasEVM1 = caller.call{gas: 2**24}(target1);
-        uint spentGasEraVM1 = gasEraVM0 - gasleft();
-
-        uint gasEraVM1 = gasleft();
-        uint spentGasEVM2 = caller.call{gas: 2**24}(target2);
-        uint spentGasEraVM2 = gasEraVM1 - gasleft();
-
-        uint gasDiffEVM = spentGasEVM2 - spentGasEVM1;
-        uint gasDiffEraVM = spentGasEraVM1 - spentGasEraVM2;
-        ratio = gasDiffEraVM / gasDiffEVM;
+        spentGasEVM = caller.call{gas: 2**24}(target);
     }
 }
