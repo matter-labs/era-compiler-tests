c23a1072fdb6a88d4f63bcb08ba2832366a94cb1
Oleksandr Zarudnyi <hedgar2017@gmail.com>
2024-05-21 19:35:04 +0200

    fix: the expected data for SC and precompiles

--- solidity/complex/yul_instructions/extcodecopy/main.sol
+++ solidity/complex/yul_instructions/extcodecopy/main.sol
@@ -5,9 +5,13 @@ pragma solidity >=0.5.0;
 contract Main {
     function main(uint256 _address) external view {
         assembly {
-            mstore(0, gt(extcodesize(_address), 0))
-            extcodecopy(_address, 32, 0, 32)
-            return(0, 64)
+            let size := extcodesize(_address)
+            mstore(0, gt(size, 0))
+            if size {
+                extcodecopy(_address, 32, 0, 32)
+                return(0, 64)
+            }
+            revert(0, 0)
         }
     }
 }
