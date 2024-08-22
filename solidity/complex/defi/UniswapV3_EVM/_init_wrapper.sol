6e498872ffa2857282ca7b66107699c1e2f744e1
Oleksandr Zarudnyi <hedgar2017@gmail.com>
2024-08-22 18:25:32 +0800

    fix: move UniswapV3 to a single folder

--- solidity/complex/defi/UniswapV3_EVM/_init_wrapper.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity =0.7.6;
-
-import './v3-core/interfaces/IUniswapV3Pool.sol';
-
-contract _InitWrapper {
-    function initialize(address pool, uint160 sqrtPriceX96) external {
-        IUniswapV3Pool(pool).initialize(sqrtPriceX96);
-    }
-}
