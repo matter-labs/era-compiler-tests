2780159327b8bae2a7bab06c60b420cd9fe38560
Oleksandr Zarudnyi <hedgar2017@gmail.com>
2024-08-27 05:17:41 +0800

    fix: block hash #3

--- solidity/ethereum/state/blockhash_basic.sol
+++ solidity/ethereum/state/blockhash_basic.sol
@@ -2,9 +2,9 @@ contract C {
     bytes32 public genesisHash;
     bytes32 public currentHash;
     constructor() {
-        require(block.number == 300);
+        require(block.number == 2);
         genesisHash = blockhash(0);
-        currentHash = blockhash(300);
+        currentHash = blockhash(2);
     }
     function f(uint blockNumber) public returns (bytes32) {
         return blockhash(blockNumber);
@@ -19,9 +19,9 @@ contract C {
 // currentHash() -> 0
 // f(uint256): 0 -> 0
 // f(uint256): 1 -> 0
-// f(uint256): 255 -> 0x3737373737373737373737373737373737373737373737373737373737373836
-// f(uint256): 256 -> 0x3737373737373737373737373737373737373737373737373737373737373837
-// f(uint256): 257 -> 0x3737373737373737373737373737373737373737373737373737373737373838
+// f(uint256): 255 -> 0x00
+// f(uint256): 256 -> 0x00
+// f(uint256): 257 -> 0x00
 // f(uint256): 300 -> 0x00
 // f(uint256): 301 -> 0x00
 // f(uint256): 302 -> 0x00
