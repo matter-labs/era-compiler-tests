79ba3421e2520f6bbf55defed8b971c6742db31b
Abinav Puthan Purayil <abinavpp@gmail.com>
2025-03-26 14:14:41 +0100

    Add 1 level inheritance test with recursive function

---
+++ solidity/simple/recursion/inheritance-1.sol
@@ -0,0 +1,29 @@
+//! { "modes": [ "Y", "E" ], "cases": [ {
+//!     "name": "f",
+//!     "inputs": [
+//!         {
+//!             "method": "f",
+//!             "calldata": [
+//!                 "1"
+//!             ]
+//!         }
+//!     ],
+//!     "expected": [
+//!         "10"
+//!     ]
+//!   }
+//! ] }
+
+pragma solidity >=0.4.12;
+
+contract P0 {
+  function f(uint a) public returns (uint) {
+    if (a == 1)
+      return 10;
+    return f(a);
+  }
+}
+contract Test is P0 {}
+
+// ----
+// f(uint256): 1 -> 10
