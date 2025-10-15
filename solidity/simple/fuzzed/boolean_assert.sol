31a433dce138a4cab2d5fb659f9ed4e160a0ddfe
PavelKopyl <PavelKopyl@users.noreply.github.com>
2025-10-15 14:18:55 +0200

    feat: add a couple of tests for a bug discovered by the fuzzer (#86)
    Co-authored-by: Oleksandr Zarudnyi <hedgar2017@gmail.com>

---
+++ solidity/simple/fuzzed/boolean_assert.sol
@@ -0,0 +1,123 @@
+//! { "modes": [ "Y", "E" ], "cases": [ {
+//!     "name": "ascending",
+//!     "inputs": [
+//!         {
+//!             "method": "check_entrypoint",
+//!             "calldata": [
+//!                 "0x80000000000000000000000000000000f876da3c85160abc0770c91f44774636",
+//!                 "0x000000000000000000000000000000003bde33efbf52406aed04e17904f90b64",
+//!                 "0x800000000000000000000000000000003ff28daac2801d83ac1d10e9aca5a5a9",
+//!                 "0x0000000000000000000000000000000000000000000000000000000000000001",
+//!                 "0x0000000000000000000000000000000000000000000000000000000000000001",
+//!                 "0x0000000000000000000000000000000000000000000000000000000000000000",
+//!                 "0x0000000000000000000000000000000000000000000000000000000000000000",
+//!                 "0x0000000000000000000000000000000000000000000000000000000000000000"
+//!             ]
+//!         }
+//!     ],
+//!     "expected": []
+//! } ] }
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.30;
+
+contract Test {
+    function $_4xN7uxIt7qtlv(
+        bool assert_in3,
+        bool assert_in5,
+        bool assert_in7
+    ) internal returns (bool) {
+        return (assert_in5 ||
+            ((true && assert_in7) && (assert_in3 && assert_in7)));
+    }
+
+    function w7Z$oKNuwzNJk78RIsb030(
+        bool assert_in3,
+        bool assert_in4,
+        bool assert_in5,
+        bool assert_in6,
+        bool assert_in7
+    ) internal returns (bool) {
+        return ((((false ||
+            (assert_in6 && (assert_in6 && (assert_in6 && (true || false))))) ||
+            (false || assert_in6)) ||
+            (false || (assert_in7 || (assert_in7 && true)))) ||
+            (((assert_in7 ||
+                (assert_in3 ||
+                    ((!(!(assert_in3 || assert_in4))) ||
+                        (!(!(assert_in4 || assert_in4)))))) &&
+                (true || (assert_in3 || assert_in4))) &&
+                (((((assert_in3 || (assert_in4 && true)) ||
+                    (assert_in3 || assert_in4)) &&
+                    ((true || true) || (assert_in4 || assert_in3))) ||
+                    (((((assert_in3 || (assert_in4 && true)) ||
+                        (assert_in3 || assert_in4)) &&
+                        (true || (assert_in4 || assert_in3))) ||
+                        (false || false)) &&
+                        ((((assert_in3 || (assert_in4 && true)) ||
+                            (assert_in3 || assert_in4)) &&
+                            (true || (assert_in4 || assert_in3))) ||
+                            (false || false)))) || assert_in5)));
+    }
+
+    function FQkY3(
+        bool assert_in3,
+        bool assert_in5,
+        bool assert_in7
+    ) internal virtual returns (bool) {
+        return ((((assert_in7 && true) ||
+            ((assert_in7 && (true && (assert_in3 && assert_in7))) ||
+                assert_in5)) ||
+            ((((assert_in7 && true) ||
+                ((assert_in7 && true) && (assert_in3 && assert_in7))) ||
+                assert_in5) && true)) &&
+            (((assert_in3 ||
+                (true && (assert_in7 && (assert_in7 && assert_in3)))) &&
+                (assert_in7 ||
+                    ((true && true) &&
+                        (assert_in7 && (assert_in3 && assert_in7))))) ||
+                assert_in5));
+    }
+
+    function Nr9GYw_(
+        bool assert_in3,
+        bool assert_in4,
+        bool assert_in5,
+        bool assert_in6,
+        bool assert_in7
+    ) internal returns (bool) {
+        return (w7Z$oKNuwzNJk78RIsb030(
+            assert_in3,
+            assert_in4,
+            assert_in5,
+            assert_in6,
+            assert_in7
+        ) || FQkY3(assert_in3, assert_in5, assert_in7));
+    }
+
+    function check_entrypoint(
+        int256 assert_in0,
+        int256 assert_in1,
+        int256 assert_in2,
+        bool assert_in3,
+        bool assert_in4,
+        bool assert_in5,
+        bool assert_in6,
+        bool assert_in7
+    ) public {
+        unchecked {
+            bool assert_out1 = ((((assert_in4 || assert_in3) &&
+                (assert_in3 || assert_in4)) ||
+                ((assert_in7 && assert_in5) || (assert_in6 || assert_in7))) ||
+                $_4xN7uxIt7qtlv(assert_in3, assert_in5, assert_in7));
+            bool assert_out2 = Nr9GYw_(
+                assert_in3,
+                assert_in4,
+                assert_in5,
+                assert_in6,
+                assert_in7
+            );
+            assert((assert_out1 == assert_out2));
+        }
+    }
+}
---
+++ solidity/simple/fuzzed/boolean_assert2.sol
@@ -0,0 +1,178 @@
+//! { "modes": [ "Y", "E" ], "cases": [ {
+//!     "name": "ascending",
+//!     "inputs": [
+//!         {
+//!             "method": "check_entrypoint",
+//!             "calldata": [
+//!                 "0x000000000000000000000000000000000728dc2c9c497b0b83d1b7d1e18130ef",
+//!                 "0x0000000000000000000000000000000000000000000000000000000000000001",
+//!                 "0x80000000000000000000000000000000adb8d2368088fd9d2fa1515a1762af84",
+//!                 "0x0000000000000000000000000000000000000000000000000000000000000001",
+//!                 "0x0000000000000000000000000000000000000000000000000000000000000001",
+//!                 "0x0000000000000000000000000000000000000000000000000000000000000000",
+//!                 "0x00000000000000000000000000000000f21b59275da3d46f622f420c823fbbf0"
+//!             ]
+//!         }
+//!     ],
+//!     "expected": []
+//! } ] }
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.30;
+
+contract Lus0bNhO0tbf41q2B {
+    int256 public state_NLAdZolVlUbLj8Hq6DkA = int256(0);
+    int256 public state_WsTyihnwA_Cl0 = int256(56);
+    int256 public state_LhNEetTl3shOb34wo = int256(46);
+
+    function HuNA5s0BSk9AU9Mf(
+        int256 assert_in0,
+        int256 assert_in6
+    ) external virtual returns (int256) {
+        return (((((((int256(block.gaslimit) - int256(100)) &
+            ((int256(block.gaslimit) - int256(100)) &
+                ((int256(block.gaslimit) - int256(100)) &
+                    (int256(block.gaslimit) - int256(100))))) +
+            (int256(100) - (state_NLAdZolVlUbLj8Hq6DkA - (-int256(0))))) -
+            ((int256(56) & int256(56)) | (int256(49) & int256(0)))) +
+            state_WsTyihnwA_Cl0) -
+            (int256(1) *
+                (state_NLAdZolVlUbLj8Hq6DkA &
+                    (state_LhNEetTl3shOb34wo - int256(0))))) %
+            ((((assert_in0 + assert_in6) | int256(0)) /
+                (
+                    -(
+                        -(((int256(44) +
+                            ((int256(block.difficulty) - int256(44)) -
+                                int256(78))) + int256(78)) /
+                            (int256(1) & int256(1)))
+                    )
+                )) &
+                (((assert_in0 - int256(64)) + (int256(64) + assert_in6)) /
+                    ((int256(44) +
+                        (int256(block.difficulty) -
+                            (-((-int256(44)) - int256(0))))) /
+                        (int256(1) & int256(1))))));
+    }
+}
+
+contract Cewtv8Gb {
+    function LJRqMhPOyMndBlhKYA2qbGZ_wSCJqYS(
+        int256 assert_out1
+    ) external returns (int256) {
+        return assert_out1;
+    }
+}
+
+contract Test {
+    function HuNA5s0BSk9AU9Mf(
+        int256 assert_in0,
+        int256 assert_in6
+    ) internal virtual returns (int256) {
+        int256[] memory local_yQIuFSh_oorvN9 = new int256[](uint256(1));
+        local_yQIuFSh_oorvN9[0] = int256(100);
+        int256[] memory local_mwQWAXVtxbrOPogK = new int256[](uint256(1));
+        local_mwQWAXVtxbrOPogK[0] = int256(46);
+        int256[] memory local_MFWW0jKKFIAf2XOMHiV6gH1_ = new int256[](
+            uint256(1)
+        );
+        local_MFWW0jKKFIAf2XOMHiV6gH1_[0] = int256(0);
+        return (((((((int256(block.gaslimit) - int256(100)) &
+            ((int256(block.gaslimit) - int256(100)) &
+                ((int256(block.gaslimit) - int256(100)) &
+                    (int256(block.gaslimit) - local_yQIuFSh_oorvN9[0])))) +
+            (int256(100) - (int256(0) - (-int256(0))))) -
+            ((int256(56) & int256(56)) | (int256(49) & int256(0)))) +
+            int256(56)) -
+            (int256(1) *
+                (int256(0) &
+                    (local_mwQWAXVtxbrOPogK[0] -
+                        local_MFWW0jKKFIAf2XOMHiV6gH1_[0])))) %
+            ((((assert_in0 + assert_in6) | int256(0)) /
+                (
+                    -(
+                        -(((int256(44) +
+                            ((int256(block.difficulty) - int256(44)) -
+                                int256(78))) + int256(78)) /
+                            (int256(1) & int256(1)))
+                    )
+                )) &
+                (((assert_in0 - int256(64)) + (int256(64) + assert_in6)) /
+                    ((int256(44) +
+                        (int256(block.difficulty) -
+                            (-((-int256(44)) - int256(0))))) /
+                        (int256(1) & int256(1))))));
+    }
+
+    function LJRqMhPOyMndBlhKYA2qbGZ_wSCJqYS(
+        int256 assert_out1
+    ) internal returns (int256) {
+        return assert_out1;
+    }
+
+    function check_entrypoint(
+        int256 assert_in0,
+        bool assert_in1,
+        int256 assert_in2,
+        bool assert_in3,
+        bool assert_in4,
+        bool assert_in5,
+        int256 assert_in6
+    ) public {
+        bool[] memory local_yOqhNgfLJE = new bool[](uint256(1));
+        local_yOqhNgfLJE[0] = false;
+        unchecked {
+            int256 assert_out1 = (
+                ((local_yOqhNgfLJE[0] || (assert_in1 || assert_in3)) &&
+                    ((assert_in3 && assert_in4) && (assert_in3 || assert_in1)))
+                    ? ((
+                        (assert_in5 || assert_in3)
+                            ? int256(block.basefee)
+                            : int256(block.chainid)
+                    ) + int256(block.basefee))
+                    : (int256(block.gaslimit) %
+                        ((assert_in0 + assert_in6) / int256(block.difficulty)))
+            );
+            int256 assert_out2 = (
+                (((!(!false)) || (assert_in1 || (!(!assert_in3)))) &&
+                    ((((assert_in1 || assert_in3) &&
+                        (assert_in3 && assert_in4)) &&
+                        (
+                            !(((!(!(!false))) ||
+                                (true &&
+                                    ((!(false || (assert_in3 || assert_in1))) ||
+                                        (
+                                            !(false ||
+                                                (assert_in1 || assert_in3))
+                                        )))) &&
+                                ((!(assert_in1 || (!(!assert_in3)))) ||
+                                    (true &&
+                                        ((
+                                            !(false ||
+                                                (assert_in3 || assert_in1))
+                                        ) ||
+                                            (
+                                                !((false || false) ||
+                                                    (assert_in1 || assert_in3))
+                                            )))))
+                        )) && (false || (assert_in1 || assert_in3))))
+                    ? (int256(block.basefee) +
+                        ((
+                            (((assert_in5 && assert_in5) || false) ||
+                                assert_in3)
+                                ? int256(block.basefee)
+                                : int256(block.chainid)
+                        ) | int256(0)))
+                    : (new Lus0bNhO0tbf41q2B()).HuNA5s0BSk9AU9Mf(
+                        assert_in0,
+                        assert_in6
+                    )
+            );
+            assert(
+                ((new Cewtv8Gb()).LJRqMhPOyMndBlhKYA2qbGZ_wSCJqYS(
+                    assert_out1
+                ) == assert_out2)
+            );
+        }
+    }
+}
